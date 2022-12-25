5c8c02abcfc9e9e62592bb7633a47b3a481f4378
yashsinghal721 <110597175+yashsinghal721@users.noreply.github.com>
2022-12-25 23:03:37 +0530

    Add files via upload

---
+++ tic-tac-toe/src/tictactoegame/Square.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Square = (props) => {
+    return (
+        <div
+            onClick={props.onClick}
+            style={{border: "1px solid",backgroundColor:"silver",height: "100px",width: "100%",display: "flex",alignItems: "center",justifyContent: "center"}}
+            className="square">
+            <h5>{props.value}</h5>
+        </div>
+    );
+}
+export default Square;
